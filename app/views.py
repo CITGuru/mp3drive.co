f4292428ee8b21345aa40883da69a4b4cc8369ad
Oyetoke Toby <oyetoketoby80@gmail.com>
2019-01-27 21:24:41 +0100

    Added Converter

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from .scraper.youtube import youtube
 # Create your views here.
 from django.contrib import messages
+from urllib.parse import parse_qs, urlsplit
 
 def home(request):
     return render(request, "index.html")
@@ -53,17 +54,29 @@ def search(request):
     }
     return render(request, "search.html", context)
 
+def extract_video_id(url):
+    urlsplt = urlsplit(url).query
+    url_queries = parse_qs(urlsplt)
+    video_id = url_queries["v"][0]
+    return video_id
+
 def convert(request):
-    query = request.GET.get("q", None)
-    if query is None:
-        query = ""
-    text = " ".join(query.split("+"))
+    video_url = request.GET["url"]
+    video_id = extract_video_id(video_url)
+
     context = {
-        "query": query,
-        "text": text,
-        "download_key": "MjcxMTg1NjMz"
+        "download_key": "MjcxMTg1NjMz",
+        "video_id":video_id,
     }
-    return render(request, "search.html", context)
+    try:
+        video_info = youtube.get_video_info(video_id)
+        video_div = youtube.get_video_source(video_id)
+        context["video"] = video_info
+        context["video_div"] = video_div
+    except:
+        messages.error(request, 'Error occured while trying to get the video source')
+    
+    return render(request, "convert.html", context)
 
 def search_youtube(request):
     if request.method == "GET":
